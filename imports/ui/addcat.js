7f79525684811a8d97a07cc9ab3b669a4761465e
rishabh jain <rishabh.jain6072@gmail.com>
2018-01-30 15:29:06 +0530

    playground

--- imports/ui/addcat.js
+++ imports/ui/addcat.js
@@ -24,25 +24,29 @@ Template.addNew.events({
 		var parentItem = e.target.parentItem.value;
 		var selectedItem = Session.get('selectedItem');
 		if(selectedItem){
+		var levelChild = Catagories.findOne({"_id": selectedItem}).level + 1;
+		}
+		if(selectedItem){
 			Catagories.insert({
 			catItem: catItem,
 			parentItem: selectedItem,
+			level: levelChild,
 			createdAt: new Date(),
 			});
 		} else {
 			Catagories.insert({
 			catItem: catItem,
 			parentItem: parentItem,
+			level: 0,
 			createdAt: new Date(),
 			});
 		}
 		 
-		// var child = [];
 		Catagories.update({"_id": parentItem} ,{$push: { "child": catItem }});
 		console.log("submit form event working!!!")
         e.target.catagory.value = "";
 	},
-	'change #sel1' : function(e) {
+	'click #sel1' : function(e) {
 		console.log("---------");
 		var selectedItem = $(e.target).val();
 		console.log(selectedItem);
@@ -59,13 +63,34 @@ Template.addNew.events({
 Template.addNew.helpers({
 	'catagory': function() {
 		var selectedItem = Session.get('selectedItem');
-		if(selectedItem){
-			return Catagories.find({"parentItem": selectedItem});
-		}
-			return Catagories.find({"parentItem": ""});
-			// return Catagories.find({});
+		// if(selectedItem){
+		// 	return Catagories.find({"parentItem": selectedItem});
+		// }
+		// 	return Catagories.find({"parentItem": ""});
+			return Catagories.find({}, { sort: {level: 1}});
 	},
-	
+
+	'selectedItem' : function() {
+		var selectedItem = Session.get('selectedItem');
+		if(selectedItem) {
+			return Catagories.findOne({"_id": selectedItem}).catItem;
+		}
+	},
+
+	'catList' : function() {
+		// var maxLevel = Catagories.find({level :{$gt : 3}});
+
+		// return Catagories.aggregate( [ { $group : { _id : "$parentItem" } } ] )
+	},
+	  treeArgs: {
+	    collection: Catagories,
+	    // subscription: subscription,
+	    events: {
+	      changed: function(e, item) {
+	        console.log("Item " + item + "selected.");
+	      }
+	    }
+	  },
 });
 
 Template.registerHelper('notequals', function (a) {
@@ -75,4 +100,3 @@ Template.registerHelper('notequals', function (a) {
 Template.registerHelper('equals', function (a, b) {
       return a === b;
     });
-
